da6ac97b066668ab77fde3e7583002de72e5d2d9
valflrt <vfleurit@gmail.com>
2022-05-12 18:08:03 +0200

    Update help.ts
    Add description auto generation in help.ts

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,28 +1,65 @@
-export default {
+import commands, { Command } from ".";
+
+/**
+ * Writes indents
+ * @param number number of indents
+ */
+let idt = (number: number = 1) => "  ".repeat(number);
+
+let help: Command = {
   name: "help",
-  matches: ["help", "h"],
+  aliases: ["h"],
+  description: "Displays this help panel",
   execute: () => {
     console.log(
-      `Commands:
-
-  - \`help\` or \`h\`
-    - Displays this help panel
-
-  - \`encrypt\` or \`e\`
-    - Encrypts a directory
-    - Options
-      --path or -p – required
-        Path of the directory to encrypt
-      --key or -k – required
-        Key used to encrypt
-
-  - \`decrypt\` or \`d\`
-    - Decrypts an encrypted directory
-    - Options
-        --path or -p – required
-          Path of the encrypted directory to decrypt
-        --key or -k – required
-          Key used to decrypt`
+      commands
+        .map((c) =>
+          idt()
+            .concat(c.name)
+            .concat(
+              c.arguments
+                ? ` ${c.arguments
+                    .map((a) => `<${a.name}${a.required === false ? "?" : ""}>`)
+                    .join(" ")}`
+                : ""
+            )
+            .concat(
+              c.description ? "\n".concat(idt(2)).concat(c.description) : ""
+            )
+            .concat("\n")
+            .concat(
+              c.aliases ? idt(2).concat(`Aliases: ${c.aliases.join(", ")}`) : ""
+            )
+            .concat(
+              c.options && c.options.length !== 0
+                ? `\n${idt(2)}Options:\n`.concat(
+                    c.options
+                      .map((o) =>
+                        idt(3)
+                          .concat(
+                            o.name.length > 1 ? `--${o.name}` : `-${o.name}`
+                          )
+                          .concat(o.description ? ` – ${o.description}` : "")
+                          .concat(
+                            o.aliases && o.aliases.length !== 0
+                              ? `\n${idt(4)}Aliases: `.concat(
+                                  o.aliases
+                                    .map((a) =>
+                                      a.length > 1 ? `--${a}` : `-${a}`
+                                    )
+                                    .join(", ")
+                                )
+                              : ""
+                          )
+                      )
+                      .join("\n")
+                  )
+                : ""
+            )
+        )
+        .join("\n\n")
     );
   },
 };
+
+export default help;
