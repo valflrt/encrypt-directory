3675d49ae94bd1d8bc2ac2e61d9ee651a1322e6f
valflrt <vfleurit@gmail.com>
2022-05-12 18:05:52 +0200

    Change command fetching system

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import { getArgs } from "./utils";
 
 let args = getArgs();
 
-let command = commands.find((c) => c.matches.some((m) => !!args._.includes(m)));
+let command = commands.find(
+  (c) => args._[0] === c.name || c.aliases.some((m) => args._[0] === m)
+);
 
 (command ? command : commands.find((c) => c.name === "help")!).execute();
