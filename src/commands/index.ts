58f705a5c8b48159f86dc30dd1ef0fea86a5fe97
valflrt <vfleurit@gmail.com>
2022-05-31 19:29:39 +0200

    Edit commands/index.ts

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,24 +1,3 @@
 // command imports
-import decrypt from "./decrypt";
-import encrypt from "./encrypt";
-import help from "./help";
-
-/**
- * Command type declaration
- */
-export interface Command {
-  name: string;
-  aliases: string[];
-  description: string;
-  arguments?: {
-    name: string;
-    description?: string;
-    required?: boolean;
-  }[];
-  options?: { name: string; aliases?: string[]; description?: string }[];
-  execute: () => any;
-}
-
-let commands: Command[] = [help, encrypt, decrypt];
-
-export default commands;
+import "./decrypt";
+import "./encrypt";
