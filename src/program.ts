04d0e1849ccc97ed2b23ff90f3a895613c98dcfa
valflrt <vfleurit@gmail.com>
2022-05-31 19:28:48 +0200

    Add program.ts
    Contains program base info

---
+++ src/program.ts
@@ -0,0 +1,7 @@
+import { program } from "commander";
+
+export default program
+  .name("crypto-vault")
+  .description("simple encryption and decryption tool.")
+  .version("1.2.0")
+  .showSuggestionAfterError(true);
