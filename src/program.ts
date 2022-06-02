0641f81bd008a8890c386cdcb77c220feb82568f
val <vfleurit@gmail.com>
2022-06-02 15:50:24 +0000

    Add verbose and debug global options

--- src/program.ts
+++ src/program.ts
@@ -4,4 +4,7 @@ export default program
   .name("crypto-vault")
   .description("simple encryption and decryption tool.")
   .version("1.2.0")
-  .showSuggestionAfterError(true);
+  .showSuggestionAfterError(true)
+
+  .option("--verbose", "verbose mode")
+  .option("--debug", "debug mode");
