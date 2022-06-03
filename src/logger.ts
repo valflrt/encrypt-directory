5fd71ad4a9d04bbffb1362e8f4166e16b4cb59b6
valflrt <vfleurit@gmail.com>
2022-06-03 19:25:12 +0200

    Rework Logger
    - Add custom behaviour depending on cli options
    - Add colored status for logs

--- src/logger.ts
+++ src/logger.ts
@@ -1,8 +1,15 @@
 import chalk from "chalk";
+import util from "util";
 
-interface LoggerCLIOptions {
+export interface LoggerCLIOptions {
   verbose?: boolean;
   debug?: boolean;
+}
+
+export interface LogMethodWithOptionsOptions {
+  items: any[];
+  verboseOnly?: boolean;
+  debugOnly?: boolean;
 }
 
 export default class Logger {
@@ -13,37 +20,108 @@ export default class Logger {
   }
 
   /**
-   * Logs info (only logs if verbose mode is enabled)
+   * Logs info
    * @param items Items to log
    */
   public info(...items: any[]) {
-    if (this._CLIOptions.verbose) {
-      let lineStart = chalk.greenBright("[INFO]").concat("  ");
-      console.log(
-        items
-          .join(" ")
-          .split(/\n/g)
-          .map((i) => lineStart.concat(i))
-          .join("\n")
-      );
+    console.log(this.baseFormat(items, "info"));
+  }
+  /**
+   * Logs info
+   * @param options Log options
+   */
+  public infoWithOptions(options: LogMethodWithOptionsOptions) {
+    if (this.logOrNo(options)) this.info(...options.items);
+  }
+
+  /**
+   * Logs warn
+   * @param items Items to log
+   */
+  public warn(...items: any[]) {
+    console.log(this.baseFormat(items, "warn"));
+  }
+  /**
+   * Logs warn
+   * @param options Log options
+   */
+  public warnWithOptions(options: LogMethodWithOptionsOptions) {
+    if (this.logOrNo(options)) this.warn(...options.items);
+  }
+
+  /**
+   * Logs error
+   * @param items Items to log
+   */
+  public error(...items: any[]) {
+    console.log(this.baseFormat(items, "error"));
+  }
+  /**
+   * Logs error
+   * @param options Log options
+   */
+  public errorWithOptions(options: LogMethodWithOptionsOptions) {
+    if (this.logOrNo(options)) this.error(...options.items);
+  }
+
+  /**
+   * Logs info
+   * @param items Items to log
+   */
+  public debug(...items: any[]) {
+    console.log(this.baseFormat(items, "debug"));
+  }
+  /**
+   * Logs info
+   * @param options Log options
+   */
+  public debugWithOptions(options: LogMethodWithOptionsOptions) {
+    if (this.logOrNo(options)) this.debug(...options.items);
+  }
+
+  /**
+   * Formats a an array before to be logged
+   * @param items Items to format
+   * @param type Type of the log
+   */
+  private baseFormat(
+    items: any[],
+    type: "info" | "warn" | "error" | "debug" = "info"
+  ) {
+    let statusText;
+    let stringStart;
+    if (type === "warn") {
+      statusText = " WARN ";
+      stringStart = chalk.yellowBright.inverse(statusText).concat(" ");
+    } else if (type === "error") {
+      statusText = " ERROR ";
+      stringStart = chalk.redBright.inverse(statusText).concat(" ");
+    } else if (type === "debug") {
+      statusText = " DEBUG ";
+      stringStart = chalk.blueBright.inverse(statusText).concat(" ");
+    } else {
+      statusText = " INFO ";
+      stringStart = chalk.greenBright.inverse(statusText).concat(" ");
     }
+
+    return stringStart.concat(
+      items
+        .map((i) => util.formatWithOptions({ colors: true }, i))
+        .join(" ")
+        .split(/\n/g)
+        .join("\n".concat(" ".repeat(statusText.length + 1)))
+    );
   }
 
   /**
-   * Logs warn (only logs if verbose mode is enabled)
-   * @param message Message to log
+   * Whether a log should be logged depending on cli options and log options
+   * @param options Options
    */
-  public warn(message: any) {
-    if (this._CLIOptions.verbose)
-      console.log(chalk.yellowBright("[WARN]").concat(`  ${message}`));
-  }
-
-  /**
-   * Logs error (only logs  if verbose mode is enabled)
-   * @param message Message to log
-   */
-  public error(message: any, plainError?: any) {
-    console.log(chalk.redBright("[ERROR]").concat(`  ${message}`));
-    if (this._CLIOptions.debug && plainError) console.error(plainError);
+  private logOrNo(options: LogMethodWithOptionsOptions) {
+    return (
+      (!options.verboseOnly && !options.debugOnly) ||
+      (this._CLIOptions.verbose && options.verboseOnly === true) ||
+      (this._CLIOptions.debug && options.debugOnly === true)
+    );
   }
 }
