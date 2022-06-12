29ddb697a854ce5e032290d728aa50f0320eff68
valflrt <vfleurit@gmail.com>
2022-06-12 10:27:58 +0200

    Edit compress-binaries.sh

--- scripts/compress-binaries.sh
+++ scripts/compress-binaries.sh
@@ -1,7 +1,5 @@
 mv binaries/fencryption-linux binaries/fencryption
 tar cvzf binaries/fencryption-linux.tar.gz binaries/fencryption
-rm binaries/fencryption
 
 mv binaries/fencryption-win.exe binaries/fencryption.exe
 tar cvzf binaries/fencryption-win.tar.gz binaries/fencryption.exe
-rm binaries/fencryption.exe
