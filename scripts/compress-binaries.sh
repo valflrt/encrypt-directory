b086a33a57fccefa2fb3b8004f5331f9661e7899
valflrt <vfleurit@gmail.com>
2022-07-07 19:20:43 +0200

    Remove compress-binaries.sh

--- scripts/compress-binaries.sh
+++
@@ -1,1 +0,0 @@
-mv binaries/fencryption-linux binaries/fencryption && tar cvzf binaries/fencryption-linux.tar.gz binaries/fencryption && mv binaries/fencryption-win.exe binaries/fencryption.exe && tar cvzf binaries/fencryption-win.tar.gz binaries/fencryption.exe 
